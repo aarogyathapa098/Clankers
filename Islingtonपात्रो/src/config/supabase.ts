a8b1f0465c99f1f69d9d8bb6a99658f044cd1960
nishkarsa <nishkarsathapamagar@gmail.com>
2026-09-12 20:32:09 +0545

    chore: initialize supabase local configuration

--- Islingtonपात्रो/src/config/supabase.ts
+++ Islingtonपात्रो/src/config/supabase.ts
@@ -1,10 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-// @ts-ignore
-declare module "@supabase/supabase-js";
-
-dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
